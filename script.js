fd94964196dc0895042969e1fee9c37da5a7f54d
Carla12344 <166743061+Carla12344@users.noreply.github.com>
2025-04-01 14:19:28 -0300

    01/04

--- script.js
+++ script.js
@@ -1,10 +1,16 @@
 document.addEventListener('DOMContentLoaded', function(){
     const aumentaFonteBotao = document.getElementById ('aumentar-fonte')
-    
+    const diminuiFonteBotao = document.getElementById ('diminuir-fonte')
+
     let tamanhoAtualFonte = 1
 
     aumentaFonteBotao.addEventListener('click', {
-        tamanhoatualfonte +=0.1;
+        tamanhoAtualFonte +=0.1;
+        document.body.style.fontSize = `${tamanhoAtualFonte,tamanhoatualfonte}rem`;
+    })
+
+    diminuiFonteBotao.addEventListener('click', {function(){
+        tamanhoatualfonte -=0.1;
         document.body.style.fontSize = `${tamanhoAtualFonte,tamanhoatualfonte}rem`;
     })
 })
