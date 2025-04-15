1f24f3d3d54e4a0ae66f369c7950114ff64590b1
Carla12344 <166743061+Carla12344@users.noreply.github.com>
2025-04-15 14:42:44 -0300

    15.04

--- script.js
+++ script.js
@@ -9,15 +9,11 @@ document.addEventListener('DOMContentLoaded', function(){
     });
 
 
-
-
-
-
-
-
     const aumentaFonteBotao = document.getElementById('aumentar-fonte');
     const diminuiFonteBotao = document.getElementById('diminuir-fonte');
+    const alternaContraste = document.getElementById('alterna-contraste');
     
+
     let tamanhoAtualFonte = 1;
     
     aumentaFonteBotao.addEventListener('click', function(){
@@ -29,4 +25,11 @@ document.addEventListener('DOMContentLoaded', function(){
     tamanhoAtualFonte -= 0.1;
     document.body.style.fontSize = `${tamanhoAtualFonte}rem`;
     });
-    });
+
+alternaContraste.addEventListener('click', function(){
+        document.body.classList.toggle('alto-contraste');
+
+
+});
+
+});
