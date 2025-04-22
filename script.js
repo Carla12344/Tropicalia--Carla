e6513993ef933ba5bf01ad012dd1b1753de319d0
Carla12344 <166743061+Carla12344@users.noreply.github.com>
2025-04-22 13:51:57 -0300

    22.04.2025

--- script.js
+++ script.js
@@ -29,7 +29,6 @@ document.addEventListener('DOMContentLoaded', function(){
 alternaContraste.addEventListener('click', function(){
         document.body.classList.toggle('alto-contraste');
 
-
 });
 
 });
